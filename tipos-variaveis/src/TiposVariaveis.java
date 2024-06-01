add5809f63f56aa514016b5183a25fbcff96c998
Beneugomes <ewerton.gomes@outlook.com.br>
2024-06-01 20:48:48 -0300

    Metodos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,10 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!!!");
+
+        final int NUMERO = 1;
+       
+
+        System.out.println(NUMERO);
     }
 }
